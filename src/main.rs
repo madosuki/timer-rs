2b33fd535bcb5be61f3bc0f219fa637acdf185fc
madosuki <yurikasamaaikatsu@gmail.com>
2023-02-13 12:24:35 +0900

    add play audio feature

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,57 @@
 use clap::Parser;
 
+use rodio::{Decoder, OutputStream, source::Source};
+
+use std::io::BufReader;
+use std::fs::File;
+use std::path::Path;
 use std::time::{Duration};
 use std::thread;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
+    // Hour, optionally.
     #[arg(short = 'H', long, default_value_t = 0)]
     hour: u64,
 
+    // Minutes, optionally.
     #[arg(short = 'm', long, default_value_t = 0)]
     minutes: u64,
 
+    // Seconds, optionally.
     #[arg(short = 's', long, default_value_t = 0)]
     seconds: u64,
     
+    // Message, output to stdout when at the specified time. Optionally.
     #[arg(short = 'M', long, default_value("時間です"))]
     message: String,
+    
+    // for play audio to the end, optionally.
+    #[arg(short = 'd', long)]
+    duration: Option<u64>,
+
+    // sound file path, optionally.
+    #[arg(short = 'f', long)]
+    file_path: Option<String>,
 }
 
-fn timer(sec: u64, msg: &str) {
-       println!("Waitineg...: {} seconds", &sec);
+fn timer(sec: u64, msg: &str, file_path_str: Option<String>, sound_duration_sec: Option<u64>) {
+    println!("Waitineg...: {} seconds", &sec);
                                                                                                         
-       thread::sleep(Duration::from_secs(sec));
-       println!("{}", msg);
+    thread::sleep(Duration::from_secs(sec));
+    println!("{}", msg);
+    
+    let Some(p_str) = file_path_str else {
+        return;
+    };
+    
+    let duration = match sound_duration_sec {
+        Some(d) => d,
+        None => 1,
+    };
+    
+    play_audio_from_file(&p_str, duration);
 }
 
 fn calc_sec(h: u64, m: u64, s: u64) -> u64 {
@@ -40,6 +68,28 @@ fn calc_sec(h: u64, m: u64, s: u64) -> u64 {
    From::from(result)
 }
 
+fn play_audio_from_file(path_str: &str, sound_sec: u64) {
+   let Ok(_r) = OutputStream::try_default() else {
+    return;
+   };
+   
+   let (_output_stream, _output_stream_handel) = _r;
+   
+   let _path = Path::new(path_str);
+   let Ok(_file) = File::open(_path) else {
+    return;
+   };
+   
+   let buf = BufReader::new(_file);
+   let Ok(_source) = Decoder::new(buf) else {
+       return;
+   };
+   
+   let _ = _output_stream_handel.play_raw(_source.convert_samples());
+   
+   std::thread::sleep(Duration::from_secs(sound_sec));
+}
+
 fn main() {
     
     let args = Args::parse();
@@ -47,8 +97,8 @@ fn main() {
     let h = args.hour;
     let m = args.minutes;
     let s = args.seconds;
-    
     let sec = calc_sec(h, m, s);
     
-    timer(sec, &args.message);
+    
+    timer(sec, &args.message, args.file_path, args.duration);
 }
